07fa589ad763aba645fdec6367a57098243ce14f
alvo254 <alvinkhamron@gmail.con>
2022-10-03 21:39:51 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/" do
+    puts "welcome to rails"
+  end
   resources :tms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
